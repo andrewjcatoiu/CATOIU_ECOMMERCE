d61d031adc0ac9f03b7fdeb33b6e99bcd2eff12d
Andrew <andrewjcatoiu26@gmail.com>
2022-09-11 15:09:48 -0700

    Solved payment cancellation error

---
+++ pages/canceled.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import { useStateContext } from '../context/StateContext';
+import toast from 'react-hot-toast';
+
+const Canceled = () => {
+    const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+    useEffect(() => {
+        localStorage.clear();
+        setCartItems([]);
+        setTotalPrice(0);
+        setTotalQuantities(0);
+    }, []);
+
+    return (
+    <div className="canceled-btn" style={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '50vh'}}>
+        <Link href="/">
+            <button type="button" width="300px" className="btn">
+                Return to Main Store
+            </button>
+        </Link>
+    </div>
+  )
+
+}
+
+export default Canceled
+
